aeeaa145b10d2af7262bb53669ff88d6134335ac
Benny Hasler <benny.h@lsports.eu>
2024-07-14 15:46:21 +0300

    feed - add base-error and validation-error classes

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  "rules": {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-throw-literal": "off",
+    "@typescript-eslint/no-throw-literal": "error"
+  }
+};
--- src/feed/vaildators/mq-settings-validar.ts
+++ src/feed/vaildators/mq-settings-validar.ts
@@ -6,6 +6,7 @@ import {
   MIN_REQUESTED_HEARTBEAT_SECONDS,
   MQSettings,
 } from "../types";
+import { ValidationError } from "../exeptions";
 
 /**
  * Class for vaildate that the configure mq setting is vaild
@@ -24,36 +25,37 @@ export class MqConnectionSettingsValidator {
       NetworkRecoveryIntervalInMs,
     } = mqSettings;
 
-    if (isNil(Host) || !isString(Host)) throw new Error("Host is required");
+    if (isNil(Host) || !isString(Host)) 
+      throw new ValidationError("Host is required and need to be string");
 
     if (isNil(Port) || !isNumber(Port) || Port <= 0)
-      throw new Error("Port must be a positive integer");
+      throw new ValidationError("Port must be a positive integer");
 
     if (isNil(VirtualHost) || !isString(VirtualHost))
-      throw new Error("VirtualHost is required");
+      throw new ValidationError("VirtualHost is required");
 
     if (isNil(PackageId) || !isNumber(PackageId) || PackageId <= 0)
-      throw new Error("PackageId must be a positive integer");
+      throw new ValidationError("PackageId must be a positive integer");
 
     if (isNil(UserName) || !isString(UserName))
-      throw new Error("UserName is required and need to be string");
+      throw new ValidationError("UserName is required and need to be string");
 
     if (isNil(Password) || !isString(Password))
-      throw new Error("Password is required and need to be string");
+      throw new ValidationError("Password is required and need to be string");
 
     if (
       isNil(PrefetchCount) ||
       !isNumber(PrefetchCount) ||
       PrefetchCount < +MIN_PREFETCH_COUNT
     )
-      throw new Error("PrefetchCount must be a positive integer");
+      throw new ValidationError("PrefetchCount must be a positive integer");
 
     if (
       isNil(RequestedHeartbeatSeconds) ||
       !isNumber(RequestedHeartbeatSeconds) ||
       RequestedHeartbeatSeconds <= +MIN_REQUESTED_HEARTBEAT_SECONDS
     )
-      throw new Error(
+      throw new ValidationError(
         "RequestedHeartbeatSeconds must be a positive integer - Larger then 10"
       );
 
@@ -62,6 +64,6 @@ export class MqConnectionSettingsValidator {
       !isNumber(NetworkRecoveryIntervalInMs) ||
       NetworkRecoveryIntervalInMs < +MIN_NETWORK_RECOVERY_INTERVAL_IN_MS
     )
-      throw new Error("NetworkRecoveryInterval must be a positive integer");
+      throw new ValidationError("NetworkRecoveryInterval must be a positive integer");
   }
 }
