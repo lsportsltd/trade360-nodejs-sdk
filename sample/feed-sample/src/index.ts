4f84649d22e6f09fa2de704fd38b6fbf7493ff0a
Benny Hasler <benny.h@lsports.eu>
2024-08-11 12:40:21 +0300

    feed - expose exceptions to feed-sample

--- sample/feed-sample/src/index.ts
+++ sample/feed-sample/src/index.ts
@@ -1,9 +1,11 @@
 import {
+  ConversionError,
   Feed,
   FixtureMetadataUpdate,
   LivescoreUpdate,
   MarketUpdate,
   SettlementUpdate,
+  ValidationError,
 } from "trade360-nodejs-sdk";
 
 import { getConfig } from "./config";
@@ -20,38 +22,50 @@ const config = getConfig();
 let logger = console;
 
 const initSample = async () => {
-  const feedInplay = new Feed(config.trade360.inplayMQSettings!, logger);
-  // const feedPrematch = Feed(config.Trade360.PrematchMQSettings, logger);
+  try {
+    const feedInplay = new Feed(config.trade360.inplayMQSettings!, logger);
+    // const feedPrematch = Feed(config.Trade360.PrematchMQSettings, logger);
 
-  // feedInplay.addEntityHandler((msg: any) => {
-  //   logger.log(`got new message:\n${JSON.stringify(msg)}\n`);
-  // });
+    // feedInplay.addEntityHandler((msg: any) => {
+    //   logger.log(`got new message:\n${JSON.stringify(msg)}\n`);
+    // });
 
-  feedInplay.addEntityHandler(
-    new FixtureMetadataUpdateHandler(),
-    FixtureMetadataUpdate
-  );
+    feedInplay.addEntityHandler(
+      new FixtureMetadataUpdateHandler(),
+      FixtureMetadataUpdate
+    );
 
-  feedInplay.addEntityHandler(new LivescoreUpdateHandler(), LivescoreUpdate);
+    feedInplay.addEntityHandler(new LivescoreUpdateHandler(), LivescoreUpdate);
 
-  feedInplay.addEntityHandler(new MarketUpdateHandler(), MarketUpdate);
+    feedInplay.addEntityHandler(new MarketUpdateHandler(), MarketUpdate);
 
-  feedInplay.addEntityHandler(new SettlementUpdateHandler(), SettlementUpdate);
+    feedInplay.addEntityHandler(
+      new SettlementUpdateHandler(),
+      SettlementUpdate
+    );
 
-  process.on("exit" || "SIGINT", async (err) => {
+    process.on("exit" || "SIGINT", async (err) => {
+      await feedInplay.stop();
+      process.exit(1);
+    });
+
+    await feedInplay.start();
+
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        return resolve();
+      }, 120 * 1000);
+    });
+
     await feedInplay.stop();
-    process.exit(1);
-  });
-
-  await feedInplay.start();
-
-  await new Promise<void>((resolve) => {
-    setTimeout(() => {
-      return resolve();
-    }, 120 * 1000);
-  });
-
-  await feedInplay.stop();
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      logger.error(`feed sample got err from ValidationError instance: ${err}`)
+    }
+    if (err instanceof ConversionError) {
+      logger.error(`feed sample got err from ConversionError instance: ${err}`)
+    }
+  }
 };
 
 initSample();
