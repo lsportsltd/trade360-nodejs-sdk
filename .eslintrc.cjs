0c7ecd0db9a3295e69106070920efaff75fffbfc
Benny Hasler <benny.h@lsports.eu>
2024-09-15 16:04:31 +0300

    config eslint in the project

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "rules": {
-    // Note: you must disable the base rule as it can report incorrect errors
-    "no-throw-literal": "off",
-    "@typescript-eslint/no-throw-literal": "error"
-  }
-};
