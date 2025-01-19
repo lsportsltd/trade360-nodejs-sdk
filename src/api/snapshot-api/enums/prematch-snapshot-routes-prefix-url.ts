32767bbf18ce226ee1b47aaeb9278664128c6504
shir.b <shir.b@lsports.eu>
2025-01-19 14:16:19 +0200

    fix codacy

--- src/api/snapshot-api/enums/prematch-snapshot-routes-prefix-url.ts
+++ src/api/snapshot-api/enums/prematch-snapshot-routes-prefix-url.ts
@@ -3,9 +3,9 @@
  */
 export enum PrematchSnapshotApiClientPrefixUrl {
   GET_EVENTS_PREFIX_URL = '/Prematch/GetEvents',
-  GET_FIXTURE_MARKETS_PREFIX_URL = '/PreMatch/GetFixtureMarkets',
-  GET_FIXTURES_PREFIX_URL = '/PreMatch/GetFixtures',
-  GET_SCORES_PREFIX_URL = '/PreMatch/GetScores',
+  GET_FIXTURE_MARKETS_PREFIX_URL = '/Prematch/GetFixtureMarkets',
+  GET_FIXTURES_PREFIX_URL = '/Prematch/GetFixtures',
+  GET_SCORES_PREFIX_URL = '/Prematch/GetScores',
   GET_OUTRGIHT_EVENT_PREFIX_URL = '/Prematch/GetOutrightEvents',
   GET_OUTRIGHT_FIXTURE_PREFIX_URL = '/Prematch/GetOutrightFixture',
   GET_OUTRIGHT_LEAGUES_PREFIX_URL = '/Prematch/GetOutrightLeagues',
