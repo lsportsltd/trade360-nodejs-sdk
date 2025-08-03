eec713df3637745c781ec09d77532dfe62871f52
noammerLS <noam.m@lsports.eu>
2025-08-03 15:39:00 +0300

    Update market-update.handler.ts

--- sample/feed-sample/src/handler/inplay/market-update.handler.ts
+++ sample/feed-sample/src/handler/inplay/market-update.handler.ts
@@ -4,7 +4,6 @@ export class MarketUpdateHandler implements IEntityHandler<MarketUpdate> {
   public processAsync = async ({ header, entity }: IMessageStructure<MarketUpdate>) => {
     console.log('MarketUpdate received!');
     console.log(entity);
-    console.log(JSON.stringify(entity));
     return;
   };
 }
