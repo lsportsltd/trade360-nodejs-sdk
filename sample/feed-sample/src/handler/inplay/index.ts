f813a68d67ac9593c53f346c7ca8b89781cd0291
shir.b <shir.b@lsports.eu>
2025-09-17 21:07:02 +0300

    Refactor feed initialization and update outright handlers
    - Changed feed initialization to start in non-play mode.
    - Updated imports in inplay handler to reference prematch handlers.
    - Added new outright handlers for settlements, fixture market, fixture, league fixture, league market, and score updates.
    - Enhanced OutrightLeagueFixtureSnapshot class to include an EndDate property with appropriate type handling.
    - Added tests for OutrightLeagueFixtureSnapshot to ensure correct deserialization and handling of EndDate.

--- sample/feed-sample/src/handler/inplay/index.ts
+++ sample/feed-sample/src/handler/inplay/index.ts
@@ -3,13 +3,13 @@ export * from './livescore-update.handler';
 export * from './market-update.handler';
 export * from './settlement-update.handler';
 
-export * from './ouright-settlements-update.handler';
-export * from './outright-fixture-market-update.handler';
-export * from './outright-fixture-update.handler';
-export * from './outright-score-update.handler';
+export * from '../prematch/ouright-settlements-update.handler';
+export * from '../prematch/outright-fixture-market-update.handler';
+export * from '../prematch/outright-fixture-update.handler';
+export * from '../prematch/outright-score-update.handler';
 
 export * from './heartbeat-update.handler';
 export * from './keep-alive-update.handler';
 
-export * from './outright-league-fixture-update.handler';
-export * from './outright-league-market-update.handler';
+export * from '../prematch/outright-league-fixture-update.handler';
+export * from '../prematch/outright-league-market-update.handler';
--- sample/feed-sample/src/handler/prematch/index.ts
+++ sample/feed-sample/src/handler/prematch/index.ts
@@ -1,1 +1,7 @@
+export * from './ouright-settlements-update.handler';
+export * from './outright-fixture-market-update.handler';
+export * from './outright-fixture-update.handler';
+export * from './outright-league-fixture-update.handler';
+export * from './outright-league-market-update.handler';
 export * from './outright-league-settlement-update.handler';
+export * from './outright-score-update.handler';
