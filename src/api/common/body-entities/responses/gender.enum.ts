cbf4206d879b34f17643f77d4697eee69708cd45
noammerLS <noam.m@lsports.eu>
2025-10-19 10:49:03 +0300

    feat(api): add participant classification enums
    - Add Gender enum (Men, Women, Mix)
    - Add AgeCategory enum (Regular, Youth, Reserves)
    - Add ParticipantType enum (Club, National, Individual, Virtual, Esports, VirtuReal, Doubles)
    
    These enums support the new Participants/Get endpoint for participant classification.

---
+++ src/api/common/body-entities/responses/gender.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * Gender enum for participant classification
+ */
+export enum Gender {
+  Men = 1,
+  Women = 2,
+  Mix = 3,
+}
+
---
+++ src/api/common/body-entities/responses/participant-type.enum.ts
@@ -0,0 +1,12 @@
+/**
+ * Participant type enum for participant classification
+ */
+export enum ParticipantType {
+  Club = 1,
+  National = 2,
+  Individual = 3,
+  Virtual = 4,
+  Esports = 5,
+  VirtuReal = 6,
+  Doubles = 7,
+}
